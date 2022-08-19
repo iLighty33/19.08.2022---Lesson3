45bdc2dcb014e566d920823973afee1558f12aa4
iLighty33 <iLighty33@gmail.com>
2022-08-19 21:10:30 +0500

    Добавьте файлы проекта.

---
+++ 19.08.2022 - Lesson3/Program.cs
@@ -0,0 +1,125 @@
+﻿// See https://aka.ms/new-console-template for more information
+using C_sharp_HW_2;
+FileInfo file = new FileInfo(@"C:\Users\З - 3\Documents\000\save.txt");
+//file.Create();
+
+List<string> strings = new List<string>();
+
+using (StreamReader reader = new(@"C:\Users\З - 3\Documents\000\save.txt"))
+{
+    string line;
+    while ((line = reader.ReadLine()) != null)
+    {
+        strings.Add(line);
+    }
+}
+
+List <Uchastniki> uchastniki = new List<Uchastniki>();
+
+//int count = Uchastniki.Vvod_age("Введите количество участников: ");
+
+//for (int i = 0; i < count; i++)
+//{
+//    uchastniki.Add(new Uchastniki(i + 1));
+//}
+
+foreach (var item in strings)
+{
+    string[] subs = item.Split('!');
+    uchastniki.Add(new Uchastniki(int.Parse(subs[0]), subs[1], int.Parse(subs[2])));
+}
+
+void vyvod()
+{
+    Console.WriteLine("Все участники:");
+
+    foreach (var item in uchastniki)
+    {
+        Console.WriteLine(item.ToString());
+    }
+}
+
+
+string s = "";
+while (s != "5")
+{
+    Console.WriteLine();
+    vyvod();
+    Console.WriteLine();
+    Console.WriteLine("[1] Добавить участника");
+    Console.WriteLine("[2] Удалить участника");
+    Console.WriteLine("[3] Изменить возраст");
+    Console.WriteLine("[4] Изменить ФИО");
+    Console.WriteLine("[5] Выход");
+
+    s = Console.ReadLine();
+    if (s == "5")
+    {
+        break;
+    }
+
+    switch (s)
+    {
+        case "1":
+            uchastniki.Add(new Uchastniki(uchastniki.Count));
+            Console.WriteLine();
+            vyvod();
+            break;
+        case "2":
+            Console.WriteLine("Введите ID участника: ");
+            uchastniki.RemoveAt(Uchastniki.Vvod_age("") - 1);
+            Console.WriteLine();
+            vyvod();
+            break;
+        case "3":
+            Console.WriteLine("Введите ID участника: ");
+            int _age = int.Parse(Console.ReadLine()) - 1;
+            Console.WriteLine("Введите новый возраст: ");
+            (uchastniki.ElementAt(_age)).Age = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            vyvod();
+            break;
+        case "4":
+            Console.WriteLine("Введите ID участника: ");
+            int _name = int.Parse(Console.ReadLine()) - 1;
+            Console.WriteLine("Введите новое ФИО: ");
+            uchastniki.ElementAt(_name).Name = String.Format(Console.ReadLine());
+            Console.WriteLine();
+            vyvod();
+            break;
+        case "5":
+
+            break;
+
+        default:
+            {
+                break;
+            }
+
+    }
+}
+
+Console.WriteLine("До свидания!!");
+
+
+
+using (StreamWriter writer = new(@"C:\Users\З - 3\Documents\000\save.txt", false))
+{
+
+    writer.WriteLine("Всё отлично работает");
+
+    Console.WriteLine("0000");
+    Console.ReadLine();
+    writer.WriteLine();
+
+    //Console.writer(item.ToString());
+    writer.Close();
+}
+
+
+
+
+
+return;
+
+    
---
+++ 19.08.2022 - Lesson3/Uchastniki.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_HW_2 {
+    internal class Uchastniki {
+        string Vvod(string text) {
+            Console.Write(text);
+            return Console.ReadLine();
+            
+        }
+
+        public static int Vvod_age(string text) {
+            try {
+                Console.Write(text);
+                return int.Parse(Console.ReadLine());
+            }
+            catch(Exception e) {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Вы ввели что-то не то, попробуйте заново.");
+                return Vvod_age(text);
+            }
+        }
+
+        public Uchastniki(int id, string name, int age) {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        public Uchastniki(int id) {
+            Id = id;
+            Name = Vvod("Введите ФИО участника: ");
+            Age = Vvod_age("Введите возраст участника: ");
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString() {
+            return $"ID Участника {Id} \nФИО {Name} \nВозраст Участника {Age}";
+        }
+    }
+}
